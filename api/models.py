aa404cbff1e3cbc74046fe099276bbb5a2b017cd
Christerpher Hunter <djhunter67@gmail.com>
2022-03-28 15:01:38 -0400

    models init

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Completed(models.Model):
+    """Save todo state as completed or not"""
+
+    completed = models.BooleanField(default=False)
+    
+
+class Todo(models.Model):
+    """Save todo"""
+
+    title = models.CharField(max_length=200, name="todo_title")
+    body = models.CharField(max_length=3000, name="todo_body")
+    completed = models.OneToOneField(Completed, on_delete=models.CASCADE, related_name="completed")
